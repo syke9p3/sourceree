49e210b02051b7e3fc7acaa9716aecf8474eb5d9
Kenth Saya-ang <kenth.sayaang.pixel8@gmail.com>
2023-12-20 11:55:48 +0800

    added mock data and guide

---
+++ data/employees.sql
@@ -0,0 +1,73 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 20, 2023 at 04:32 AM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `playground`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `employees`
+--
+
+CREATE TABLE `employees` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `department` varchar(255) DEFAULT NULL,
+  `position` varchar(255) DEFAULT NULL,
+  `salary` decimal(10,2) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `employees`
+--
+
+INSERT INTO `employees` (`id`, `name`, `email`, `department`, `position`, `salary`) VALUES
+(1, 'John Doe', 'john@example.com', 'Engineering', 'Software Developer', '80000.00'),
+(2, 'Jane Smith', 'jane@example.com', 'Marketing', 'Marketing Manager', '90000.00'),
+(3, 'Alex Johnson', 'alex@example.com', 'Human Resources', 'HR Specialist', '75000.00'),
+(4, 'Emily Brown', 'emily@example.com', 'Finance', 'Financial Analyst', '85000.00'),
+(5, 'Michael Lee', 'michael@example.com', 'Operations', 'Operations Manager', '95000.00');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `employees`
+--
+ALTER TABLE `employees`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `employees`
+--
+ALTER TABLE `employees`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ guide.md
@@ -0,0 +1,16 @@
+Hello guys. Eto ay isang testing na ginawa ko lang para mag-setup ng isang simple server at pagdidisplay ng data sa html.
+
+Para mapagana to:
+
+1. Magbukas ng terminal, type ` npm install `
+
+2. Gumawa ng mock data sa mysql
+    - open mysql sa phpmyadmin gamit xampp
+    - create database named 'playground'
+    - gamitin yung *employees.sql* sa data folder
+
+3. Open server
+    - In another terminal, type ` npm run dev `
+
+4. Open html
+    - use five server or kung pano kayo magbukas ng html ang src/index.html
